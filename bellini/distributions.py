53b4da6af31598b74d1ee6e53886fe404d5abe48
Yuanqing Wang <wangyq@wangyq.net>
2020-10-12 00:01:01 -0400

    distribution algebra

--- bellini/distributions.py
+++ bellini/distributions.py
@@ -37,10 +37,54 @@ class Distribution(abc.ABC):
     def magnitude(self):
         raise NotImplementedError
 
+    def __add__(self, x):
+        return ComposedDistribution([self, x], operator="add")
+
+    def __sub__(self, x):
+        return ComposedDistribution([self, x], operator="sub")
+
+    def __mul__(self, x):
+        return ComposedDistribution([self, x], operator="mul")
+
+    def __div__(self, x):
+        return ComposedDistribution([self, x], operator="div")
+
+    def __pow__(self, x):
+        return TransformedDistribution(self, operator="pow", order=x)
+
+    def __abs__(self):
+        return TransformedDistribution(self, operator="abs")
+
+    def exp(self):
+        # TODO: re-write math
+        return TransformedDistribution(self, operator="exp")
+
+    def __exp__(self):
+        return self.exp()
+
+    def log(self):
+        # TODO: re-write math
+        return TransformedDistribution(self, operator="exp")
+
+    def __log__(self):
+        return self.log()
+
 class ComposedDistribution(Distribution):
     """ A composed distribution made of two distributions. """
     def __init__(self, distributions, operator):
         super(ComposedDistribution, self).__init__()
+        self.distributions = distributions
+        self.operator = operator
+
+class TransformedDistribution(Distribution):
+    """ A transformed distribution from one base distribution. """
+    def __init__(self, distribution, operator, **kwargs):
+        super(TransformedDistribution, self).__init__()
+        self.distribution = distribution
+        self.operator = operator
+        for key, value in kwargs:
+            setattr(self, key, value)
+
 
 # =============================================================================
 # MODULE CLASSES
