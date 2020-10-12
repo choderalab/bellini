b4bec25a81dbfb665a8ec919c211084cbbd2c5ed
Yuanqing Wang <wangyq@wangyq.net>
2020-10-12 10:53:22 -0400

    story test

--- bellini/tests/test_story.py
+++ bellini/tests/test_story.py
@@ -33,3 +33,21 @@ def test_construct():
         species=water,
         moles=Quantity(2.0, ureg.mole)
     )
+
+def test_pipeline():
+    import bellini
+    from bellini import Quantity, Species, Substance, Story
+    import pint
+    ureg = pint.UnitRegistry()
+
+    s = Story()
+    water = Species(name='water')
+    s.one_water_quantity = bellini.distributions.Normal(
+        loc=Quantity(3.0, ureg.mole),
+        scale=Quantity(0.01, ureg.mole),
+    )
+    s.another_water_quantity = bellini.distributions.Normal(
+        loc=Quantity(3.0, ureg.mole),
+        scale=Quantity(0.01, ureg.mole),
+    )
+    s.combined_water = s.one_water_quantity + s.another_water_quantity
