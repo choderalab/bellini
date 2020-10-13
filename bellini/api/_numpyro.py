14e283f1e516a1e0cd4bc460f848ad2aa83b3d90
Yuanqing Wang <wangyq@wangyq.net>
2020-10-13 16:53:08 -0400

    start to brainstorm about numpyro api

---
+++ bellini/api/_numpyro.py
@@ -0,0 +1,11 @@
+# =============================================================================
+# IMPORTS
+# =============================================================================
+import numpyro
+import bellini
+
+def story_to_numpyro_model(story):
+    """ Convert a `bellini.Story` to a `numpyro` model. """
+    
+
+    raise NotImplementedError
